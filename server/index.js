0adb60779c73c299d2182adb579e016e6ee193dc
Nina Tan <ninavtan@gmail.com>
2022-01-13 14:09:11 -0500

    Fixed missing path module in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path');
+
 
 mongoose.connect("mongodb://localhost/agile", {
   useNewUrlParser: true,
