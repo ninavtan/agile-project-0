12f2c4bbd5f70d005e03a40e046b05492e8ae65d
Kush Patel <knp314@gmail.com>
2022-01-14 23:10:14 -0500

    Added board reducer, fetchBoards action, and fixed board margin CSS

--- client/src/components/actions/types.js
+++ client/src/components/actions/types.js
@@ -1,3 +1,4 @@
 export const UPDATE_LIST_ORDER = 'update_list_order'
 export const MOVE_TASK_WITHIN_LIST = 'move_task_within_list';
 export const MOVE_TASK_BETWEEN_LISTS = 'move_task_between_lists';
+export const FETCH_BOARDS = 'fetch_boards';
