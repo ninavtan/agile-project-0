5a8e11c5f1e517dd5d1b73f634fb2028a6e13c1d
Nina Tan <ninavtan@gmail.com>
2022-01-21 14:00:52 -0500

    Added redux devtools to examine the store

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import Board from './components/board';
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+// Redux Devtools Configuration
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk)));
 
 // const App = (props) => {
 const App = () => {
