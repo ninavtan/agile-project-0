4b5bdb2f1a7ce6913c2ee1e20bc1879958a68267
Nina Tan <ninavtan@gmail.com>
2022-02-01 13:22:46 -0500

    Added Express serve-production-assets to index.js and Heroku is not liking that. Removing now.

--- server/index.js
+++ server/index.js
@@ -4,18 +4,18 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const keys = require('./config/keys');
 
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Express will serve up production assets
+//   // like our main.js file, or main.css file!
+//   app.use(express.static("client/build"));
 
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // mongoose.connect("mongodb://localhost/agile", {
 //   useNewUrlParser: true,
