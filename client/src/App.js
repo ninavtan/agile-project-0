bd1d2ca700c8f5c1cc8e87b7ad4eb0c70ce6a42c
Nina Tan <ninavtan@gmail.com>
2022-02-01 11:42:02 -0500

    Styling changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import { Provider, useSelector } from 'react-redux';
-import { createStore, applyMiddleware, compose } from "redux";
-import { Routes, Route, Navigate } from "react-router-dom";
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
+import { useSelector } from 'react-redux';
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Board from './components/board';
 import Home from './components/auth/home';
 import Auth from './components/auth/auth';
@@ -18,27 +15,24 @@ import NavBar from './components/navbar';
     // if currentUser is true, set auth to true.
 
     console.log('Is the user logged in?' + currentUser.isLoggedIn);
-    useEffect(() => {if (currentUser.isLoggedIn) {
-      setAuth(true);
-    } else {
-      setAuth(false);
-    }}, []);
+    // useEffect(() => {if (currentUser.isLoggedIn) {
+    //   setAuth(true);
+    // } else {
+    //   setAuth(false);
+    // }}, []);
 
     // Gets the 'user' item from localStorage to check if the session is persisting.
-    // useEffect(() => {
-    //   let user = localStorage.getItem("user");
-    //   user && JSON.parse(user) ? setAuth(true) : setAuth(false);
-    // }, []);
+    useEffect(() => {
+      let user = localStorage.getItem("user");
+      user && JSON.parse(user) ? setAuth(true) : setAuth(false);
+    }, []);
   
     // If auth is true, sets a session in localStorage.
-    // useEffect(() => {
-    //   localStorage.setItem("user", auth);
-    // }, [currentUser, auth]);
+    useEffect(() => {
+      localStorage.setItem("user", auth);
+    }, [currentUser, auth]);
   
     return (
-      // <Provider store={store}>
-      <>
-
       <Routes>
         {!auth && (
           <Route
@@ -50,17 +44,38 @@ import NavBar from './components/navbar';
         {auth && (
           <>
             <Route
-              path="/home"
+              path="/boards"
               element={<Home logout={() => setAuth(false)} />}
-            />
-            <Route path="/boards" element={<Board />} />
+            >
+            
+               <Route path=":boardId" element={<Board />} />
+              
+
+            </Route>
+            {/* <Route path="/boards" element={<BoardsHome />} > */}
+             
+            
           </>
         )}
-        <Route path="*" element={<Navigate to={auth ? "/home" : "/auth"} />} />
+        <Route path="*" 
+        //  element={NoMatch()}
+        
+        element=
+        {<Navigate to={auth ? "/boards" : "/auth"}/>}
+        // element={
+        //   <main style={{ padding: "1rem" }}>
+        //     <p>There's nothing here!</p>
+        //   </main>
+        // }
+         />
+       
+
       </Routes>
-    </>
+  
 
     );
   };
-  
+
   export default App;
+
+  
--- client/src/components/auth/home.js
+++ client/src/components/auth/home.js
@@ -1,57 +1,76 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import styled from 'styled-components';
 import { Row, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { userLogout } from "../actions";
+import { userLogout, fetchUserBoards } from "../actions";
 import RenderUserBoards from "./RenderUserBoards";
+import "../../App.css";
 
 // userLogout functionality is not working atm. When you refresh the page, it will trigger you to log in again.
 
 const Home = ({ logout }) => {
-  const currentUser = useSelector(state => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
- 
+
+  const currentUser = useSelector(state => state.user);
+
+  const boards = useSelector(state => state.boards.allBoards);
+
+  // console.log(boards);
+  let arrayifiedBoards = Object.entries(boards);
+  console.log(arrayifiedBoards);
+  
   useEffect(() => {
     checkUser();
+    // This grabs all the user's boards after they log in.
+    dispatch(fetchUserBoards(currentUser._id));
   }, [currentUser.isLoggedIn]);
 
   const checkUser = () => {
     if (!currentUser.isLoggedIn) {
       logout();
       navigate("home");
-    } 
+    }
   }
 
   return (
-    <HomeContainer>
-      <Row>
-        <Link to="/boards">Main Board</Link>
-      </Row>
-
+    <div id="boardContainer">
       <h1> Hi {currentUser.username}, you are logged in.</h1>
       {/* <StyledButton onClick={handleLogoutClick}>Logout</StyledButton> */}
-      <RenderUserBoards currentUser={currentUser} />
 
-    </HomeContainer>
+        
+      {boards.map(board => (
+        <div id="userBoardContainer">
+          <Link
+            class="boardLinks"
+            style={{ margin: "1rem" }}  
+            to={`/boards/${board._id}`}>
+
+            <BoardSquare>
+              {board.title}
+            </BoardSquare>
+          </Link>
+        </div>
+      ))}
+    
+    <Outlet/>
+    </div>
+    
   );
 };
 
 export default Home;
 
-const HomeContainer = styled.div`
- margin: 0 auto;
-  width: 80vw;
-  display: block;
-  border: 1px solid lightgrey;
-  border-radius: 5px;
-  background-color: #ebecf0;
-  font-family: sans-serif;
-  text-align: center;
+const BoardSquare = styled.div`
+ width: 200px;
+ height: 100px;
+ margin: 1em;
+ display: inline-block;
+border-radius: 3px;
+padding: 1em;
+
 `;
 
-const StyledButton = styled(Button)`
-margin: 8px;
-`
+
