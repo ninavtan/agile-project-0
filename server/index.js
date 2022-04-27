308bc8bf5d20807d3601864f570ce4dd3be834e1
Nina Tan <ninavtan@gmail.com>
2022-04-27 18:54:36 -0400

    Pushing build files to heroku again

--- server/index.js
+++ server/index.js
@@ -5,16 +5,6 @@ const path = require('path');
 const keys = require('./config/keys');
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // DB Setup
 mongoose.connect(keys.MONGODB_URI, {
   useNewUrlParser: true,
