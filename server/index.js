3cddc920342cba7f8aebf3ea2c5923aa368d456e
Nina Tan <ninavtan@gmail.com>
2022-04-27 19:11:17 -0400

    Fixing 404 error on heroku when trying to access API

--- server/index.js
+++ server/index.js
@@ -4,15 +4,7 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const keys = require('./config/keys');
 const app = express();
-
-if (process.env.NODE_ENV === "production") {
-
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+const cors = require("cors");
 
 // DB Setup
 mongoose.connect(keys.MONGODB_URI, {
@@ -25,6 +17,9 @@ mongoose.connect(keys.MONGODB_URI, {
 
 const PORT = process.env.PORT || 7000;
 
+app.use(cors({
+  origin: '*'
+}));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -49,6 +44,15 @@ var authRouter = require('./routes/auth');
 
 app.use(mainRoutes);
 app.use('/', authRouter);
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
